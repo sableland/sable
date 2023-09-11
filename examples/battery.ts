ef9c86d1f36c224e072f8a008a84299d8610fe4b
lino-levan <11367844+lino-levan@users.noreply.github.com>
2023-09-12 08:23:47 +1200

    feat: add events to battery api

--- examples/battery.ts
+++ examples/battery.ts
@@ -4,3 +4,19 @@ console.log(battery.charging);
 console.log(battery.chargingTime);
 console.log(battery.dischargingTime);
 console.log(battery.level);
+
+battery.onchargingchange = () => {
+  console.log("Charging change", battery.charging);
+};
+
+battery.onchargingtimechange = () => {
+  console.log("Charging time change", battery.chargingTime);
+};
+
+battery.ondischargingtimechange = () => {
+  console.log("Discharging time change", battery.dischargingTime);
+};
+
+battery.onlevelchange = () => {
+  console.log("Level change", battery.level);
+};
--- ext/battery/mod.js
+++ ext/battery/mod.js
@@ -23,6 +23,39 @@ let batteryManager;
 globalThis.navigator.getBattery = async () => {
   if (!batteryManager) {
     batteryManager = new BatteryManager();
+
+    let oldCharging = batteryManager.charging;
+    let oldChargingTime = batteryManager.chargingTime;
+    let oldDischargingTime = batteryManager.dischargingTime;
+    let oldLevel = batteryManager.level;
+
+    setInterval(() => {
+      const newCharging = batteryManager.charging;
+      const newChargingTime = batteryManager.chargingTime;
+      const newDischargingTime = batteryManager.dischargingTime;
+      const newLevel = batteryManager.level;
+
+      if (oldCharging !== newCharging) {
+        batteryManager?.onchargingchange();
+        batteryManager.dispatchEvent(new Event("chargingchange"));
+      }
+      if (oldChargingTime !== newChargingTime) {
+        batteryManager?.onchargingtimechange();
+        batteryManager.dispatchEvent(new Event("chargingtimechange"));
+      }
+      if (oldDischargingTime !== newDischargingTime) {
+        batteryManager?.ondischargingtimechange();
+        batteryManager.dispatchEvent(new Event("dischargingtimechange"));
+      }
+      if (oldLevel !== newLevel) {
+        batteryManager?.onlevelchange();
+        batteryManager.dispatchEvent(new Event("levelchange"));
+      }
+      oldCharging = newCharging;
+      oldChargingTime = newChargingTime;
+      oldDischargingTime = newDischargingTime;
+      oldLevel = newLevel;
+    }, 1000);
   }
   return batteryManager;
 };
