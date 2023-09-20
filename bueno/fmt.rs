681999b0935ef84b318578e74e66dfc6792484c2
lino-levan <11367844+lino-levan@users.noreply.github.com>
2023-09-20 16:23:26 -0700

    feat: add support for all the other file formats

--- bueno/fmt.rs
+++ bueno/fmt.rs
@@ -1,13 +1,53 @@
 use deno_core::error::AnyError;
-use dprint_plugin_typescript::configuration::ConfigurationBuilder;
+use dprint_plugin_json;
+use dprint_plugin_markdown;
+use dprint_plugin_typescript;
 use glob::glob;
 use std::ffi::OsStr;
+use std::path::{Path, PathBuf};
 
-// fn format_file(path: &str) -> Result<(), AnyError> {
-//   let contents = std::fs::read_to_string(path)?;
-//   println!("fmt: {}", path);
-//   Ok(())
-// }
+fn fake_path(ext: &str) -> PathBuf {
+    let file_name = format!("file.{}", ext);
+    PathBuf::from(file_name)
+}
+
+fn format_typescript_file(path: &Path, contents: &str) -> Result<Option<String>, AnyError> {
+    dprint_plugin_typescript::format_text(
+        path,
+        contents,
+        &dprint_plugin_typescript::configuration::ConfigurationBuilder::new()
+            .line_width(80)
+            .build(),
+    )
+}
+
+fn format_json_file(contents: &str) -> Result<Option<String>, AnyError> {
+    dprint_plugin_json::format_text(
+        &contents,
+        &dprint_plugin_json::configuration::ConfigurationBuilder::new()
+            .line_width(80)
+            .build(),
+    )
+}
+
+fn format_markdown_file(contents: &str) -> Result<Option<String>, AnyError> {
+    dprint_plugin_markdown::format_text(
+        &contents,
+        &dprint_plugin_markdown::configuration::ConfigurationBuilder::new()
+            .line_width(80)
+            .build(),
+        |tag, text, _line_number| format_file(tag, text),
+    )
+}
+
+fn format_file(ext: &str, contents: &str) -> Result<Option<String>, AnyError> {
+    match ext {
+        "js" | "ts" | "jsx" | "tsx" => format_typescript_file(fake_path(ext).as_path(), &contents),
+        "json" | "jsonc" => format_json_file(&contents),
+        "md" | "markdown" => format_markdown_file(&contents),
+        _ => Ok(None),
+    }
+}
 
 pub struct FormatOptions<'a> {
     pub check: bool,
@@ -18,25 +58,19 @@ pub fn fmt(options: FormatOptions) -> Result<(), AnyError> {
     for entry in glob(&options.glob)? {
         match entry {
             Ok(path) => {
-                if let Some(ext) = path.extension().and_then(OsStr::to_str) {
-                    match ext {
-                        "js" | "ts" | "jsx" | "tsx" => {
-                            let contents = std::fs::read_to_string(path.clone())?;
+                match path.extension().and_then(OsStr::to_str) {
+                    Some(
+                        ext @ ("js" | "ts" | "jsx" | "tsx" | "json" | "jsonc" | "md" | "markdown"),
+                    ) => {
+                        let contents = std::fs::read_to_string(path.clone())?;
 
-                            let result = dprint_plugin_typescript::format_text(
-                                &path,
-                                &contents,
-                                &ConfigurationBuilder::new().line_width(80).build(),
-                            )?;
-
-                            if let Some(text) = result {
-                                println!("fmt: {}", path.display());
-                                // print out formatted text
-                                std::fs::write(path, text)?;
-                            }
+                        if let Some(text) = format_file(ext, &contents)? {
+                            println!("fmt: {}", path.display());
+                            // print out formatted text
+                            std::fs::write(path, text)?;
                         }
-                        _ => {}
                     }
+                    _ => {}
                 }
             }
             Err(e) => println!("{:?}", e),
