96893e2369940f2f479f93e4f8ca14309cf4669f
Im-Beast <franik.mateusz@gmail.com>
2024-03-16 17:57:43 +0100

    feat: overhaul module loader

--- bueno/module_cache.rs
+++ bueno/module_cache.rs
@@ -1,6 +1,9 @@
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
-use deno_core::{anyhow::bail, error::AnyError, ModuleSpecifier};
+use crate::utils::fs::atomic_write;
+use deno_core::anyhow::{bail, Error};
+use deno_core::{ModuleSourceCode, ModuleSpecifier};
+use tokio::fs;
 
 pub fn module_specifier_to_path_buf(module_specifier: &ModuleSpecifier) -> PathBuf {
     PathBuf::from(
@@ -12,34 +15,50 @@ pub fn module_specifier_to_path_buf(module_specifier: &ModuleSpecifier) -> PathB
 }
 
 pub struct ModuleCache {
-    pub cache_location: PathBuf,
+    location: PathBuf,
 }
 
 impl ModuleCache {
-    pub fn add(&self, module_specifier: &ModuleSpecifier, code: String) -> Result<(), AnyError> {
+    pub fn new(location: PathBuf) -> Self {
+        if !location.is_absolute() || location.parent().is_none() {
+            panic!("ModuleCache location must be an absolute path with at least one parent")
+        }
+        Self { location }
+    }
+
+    pub fn location(&self) -> &Path {
+        &self.location
+    }
+
+    pub async fn add(
+        &self,
+        module_specifier: &ModuleSpecifier,
+        source_code: &ModuleSourceCode,
+    ) -> Result<(), Error> {
         println!("Adding {} to cache", module_specifier);
 
-        let cache_location = self.cache_location.clone();
-
+        let cache_location = self.location();
         let url_path = module_specifier_to_path_buf(module_specifier);
-        let cache_path = cache_location.join(url_path);
 
-        let parent = cache_path
-            .parent()
-            .expect("Failed getting a cache_path parent");
+        let cache_path = cache_location.join(url_path);
+        let parent = cache_path.parent().unwrap();
 
         if !parent.exists() {
-            std::fs::create_dir_all(parent)?;
+            fs::create_dir_all(parent).await?;
         }
 
-        std::fs::write(cache_path, code)?;
+        let bytes = match source_code {
+            ModuleSourceCode::Bytes(bytes) => bytes.as_bytes(),
+            ModuleSourceCode::String(string) => string.as_bytes(),
+        };
+        atomic_write(&cache_path, bytes).await?;
 
         Ok(())
     }
 
-    pub fn get(&self, module_specifier: &ModuleSpecifier) -> Result<String, AnyError> {
+    pub async fn get(&self, module_specifier: &ModuleSpecifier) -> Result<Box<[u8]>, Error> {
         let url_path = module_specifier_to_path_buf(module_specifier);
-        let cache_path = self.cache_location.join(url_path);
+        let cache_path = self.location.join(url_path);
 
         if !cache_path.exists() {
             bail!(
@@ -49,18 +68,21 @@ impl ModuleCache {
             );
         }
 
-        let code = std::fs::read_to_string(cache_path)?;
-        Ok(code)
+        let code = fs::read(cache_path).await?;
+        Ok(code.into_boxed_slice())
     }
 
-    pub fn clear(&self) -> Result<(), AnyError> {
-        let cache_path = self.cache_location.clone();
-
+    pub async fn clear(&self) -> Result<(), Error> {
+        let cache_path = &self.location;
+        println!("Clearing cache at {}", cache_path.display());
         if !cache_path.exists() {
+            println!("Cache is already empty");
             return Ok(());
         }
 
-        std::fs::remove_dir_all(cache_path)?;
+        println!("Removing cache at {}", cache_path.display());
+        fs::remove_dir_all(cache_path).await?;
+
         Ok(())
     }
 }
