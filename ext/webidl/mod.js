69f732ccfe12ab65fe3936f183daf5d671fffb33
Andreu Botella <andreu@andreubotella.com>
2024-03-16 23:09:02 +0100

    Implement Event and EventTarget according to the spec (#48)

--- ext/webidl/mod.js
+++ ext/webidl/mod.js
@@ -1,1 +1,44 @@
 export * from "ext:sable/webidl/numbers.js";
+
+/**
+ * Defines WebIDL constants on an interface, namespace, etc.
+ *
+ * https://webidl.spec.whatwg.org/#define-the-constants
+ *
+ * @param {object | object[]} targets - The interface, prototype, etc.
+ * on which to define the constants. Can be an array, to define
+ * constants on an interface's constructor and prototype at the same
+ * time.
+ * @param {Record<string, any>} constants
+ */
+export function defineConstants(targets, constants) {
+	/** @type {PropertyDescriptorMap} */
+	const descriptors = {};
+	for (const [constantName, value] of Object.entries(constants)) {
+		descriptors[constantName] = {
+			writable: false,
+			enumerable: true,
+			configurable: false,
+			value
+		};
+	}
+
+	const targetArray = Array.isArray(targets) ? targets : [targets];
+	for (const target of targetArray) {
+		Object.defineProperties(target, descriptors);
+	}
+}
+
+/**
+ * Defines WebIDL constants on an interface. This function handles
+ * both the constructor and the prototype.
+ *
+ * https://webidl.spec.whatwg.org/#define-the-constants
+ *
+ * @param { {prototype: object} } target - A WebIDL interface
+ * constructor.
+ * @param {Record<string, any>} constants
+ */
+export function defineInterfaceConstants(target, constants) {
+	defineConstants([target, target.prototype], constants);
+}
