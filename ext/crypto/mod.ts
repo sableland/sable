cdbd6cd36846f6e138c2dc41b2529c59bbeca3db
Lino Le Van <11367844+lino-levan@users.noreply.github.com>
2024-04-07 08:32:05 -0700

    fix: beast nits

--- ext/crypto/mod.ts
+++ ext/crypto/mod.ts
@@ -2,8 +2,20 @@ import {
   op_crypto_new_uuidv4
 } from "ext:core/ops";
 
-globalThis.crypto = {
+class Crypto {
+  static #created = false;
+  constructor() {
+    if (Crypto.#created) {
+      throw new TypeError("Illegal constructor");
+    }
+    Crypto.#created = true;
+  }
+
   randomUUID() {
     return op_crypto_new_uuidv4();
   }
 }
+
+globalThis.Crypto = Crypto;
+
+globalThis.crypto = new Crypto();
--- tests/crypto.test.js
+++ tests/crypto.test.js
@@ -1,5 +1,10 @@
 const { test } = Sable.testing;
 
+test("Crypto API - Crypto is unconstructable", (ctx) => {
+  ctx.throws(() => new Crypto(), TypeError);
+});
+
+
 test("Crypto API - randomUUID", (ctx) => {
   ctx.assert(typeof crypto.randomUUID() === "string");
 	ctx.assert(crypto.randomUUID().length === 36);
