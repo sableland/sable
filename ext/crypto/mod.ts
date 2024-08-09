25e994de84cee75260bd7b245db9ba310274240f
Mateusz Franik <47059999+Im-Beast@users.noreply.github.com>
2024-08-09 17:56:17 +0200

    fix: race condition in `sable fmt` (#62)

--- ext/crypto/mod.ts
+++ ext/crypto/mod.ts
@@ -1,19 +1,17 @@
-import {
-  op_crypto_new_uuidv4
-} from "ext:core/ops";
+import { op_crypto_new_uuidv4 } from "ext:core/ops";
 
 class Crypto {
-  static #created = false;
-  constructor() {
-    if (Crypto.#created) {
-      throw new TypeError("Illegal constructor");
-    }
-    Crypto.#created = true;
-  }
+	static #created = false;
+	constructor() {
+		if (Crypto.#created) {
+			throw new TypeError("Illegal constructor");
+		}
+		Crypto.#created = true;
+	}
 
-  randomUUID() {
-    return op_crypto_new_uuidv4();
-  }
+	randomUUID() {
+		return op_crypto_new_uuidv4();
+	}
 }
 
 globalThis.Crypto = Crypto;
